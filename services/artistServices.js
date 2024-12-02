418b9b1f3ee38663fb72a04c30005277ae425d9e
AndersonFBD <andersonf.barrosdias@gmail.com>
2024-12-02 20:30:36 -0300

    feat: add artist service file and list all artists services

---
+++ services/artistServices.js
@@ -0,0 +1,22 @@
+const fs = require("fs").promises;
+const path = require("path");
+const jwt = require("jsonwebtoken");
+
+const filepath = path.join(__dirname, "../data", "artists.json");
+
+const readArtistFile = async () => {
+  try {
+    const data = await fs.readFile(filepath, "utf-8");
+    return JSON.parse(data);
+  } catch (err) {
+    console.error("Erro na leitura na base de dados:", err);
+    throw err;
+  }
+};
+
+exports.getAllArtists = async (req, res) => {
+  const allArtists = await readArtistFile();
+  res.status(200).JSON(allArtists);
+};
+
+exports.getArtist = (id) => {};
