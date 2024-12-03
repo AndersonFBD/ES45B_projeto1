8b7efd88d5f1240c9ace082e59ca073260423416
AndersonFBD <andersonf.barrosdias@gmail.com>
2024-12-03 00:45:12 -0300

    feat: add artist services for update and delete requisitions

--- services/artistServices.js
+++ services/artistServices.js
@@ -43,12 +43,53 @@ exports.addNewArtist = async (artist) => {
   let id = Number(allArtists.length) + 1;
   console.log(artist);
   const newEntry = { id, ...artist };
-  userList.push(newEntry);
+  artistCollection.push(newEntry);
 
   try {
     await fs.writeFile(filepath, JSON.stringify(allArtists), "utf-8");
     return newEntry;
   } catch (err) {
     console.error(err);
+  }
+};
+
+exports.editArtist = async (id, editedArtist) => {
+  let artistCollection = await readArtistFile();
+  const artist_index = artistCollection.findIndex(
+    (artist) => artist.id === Number(id)
+  );
+  if (artist_index === -1) {
+    return null;
+  }
+  artistCollection[artist_index] = {
+    ...artistCollection[artist_index],
+    ...editedArtist,
+  };
+
+  try {
+    await fs.writeFile(filepath, JSON.stringify(artistCollection), "utf-8");
+    return artistCollection[artist_index];
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+exports.removeArtist = async (id) => {
+  let artistCollection = await readArtistFile();
+  const artist_index = artistCollection.findIndex(
+    (artist) => artist.id === Number(id)
+  );
+  if (artist_index === -1) {
+    return null;
+  }
+  let removedEntry = artistCollection[artist_index];
+  artistCollection.splice(artist_index, 1)[0];
+
+  try {
+    await fs.writeFile(filepath, JSON.stringify(artistCollection), "utf-8");
+    return removedEntry;
+  } catch (err) {
+    console.error(err);
+    return null;
   }
 };
