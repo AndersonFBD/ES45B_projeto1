2a446a178a21d431ae63a0c4e98c255f0a23752d
AndersonFBD <andersonf.barrosdias@gmail.com>
2024-12-09 18:57:04 -0300

    feat: attached artist validation to artist routes

--- routes/artistRoutes.js
+++ routes/artistRoutes.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const artistController = require("../controllers/artistController");
 const { verifyCredentials } = require("../middlewares/verifyCredentials");
-
+const { validateArtist } = require("../validations/artistValidator");
 const router = express.Router();
 
 //apenas usuarios logados podem adicionar, editar ou remover artistas
 router.get("/all", artistController.getAll);
 router.get("/:id", artistController.findOneArtist);
-router.post("/", verifyCredentials, artistController.addArtist);
-router.put("/:id", verifyCredentials, artistController.updateArtist);
+router.post("/", verifyCredentials, validateArtist, artistController.addArtist);
+router.put(
+  "/:id",
+  verifyCredentials,
+  validateArtist,
+  artistController.updateArtist
+);
 router.delete("/:id", verifyCredentials, artistController.deleteArtist);
 
 module.exports = router;
